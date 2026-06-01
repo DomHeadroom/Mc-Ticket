b5c32c911e8d28abaeaff2e0d703aa93502571fe
DomHeadroom <domefemia@protonmail.com>
2026-06-01 17:01:11 +0200

    Init Frontend e Backend
    Signed-off-by: DomHeadroom <domefemia@protonmail.com>

---
+++ backend/src/main/java/it/domheadroom/mc_ticket/McTicketApplication.java
@@ -0,0 +1,13 @@
+package it.domheadroom.mc_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McTicketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/domheadroom/mc_ticket/McTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package it.domheadroom.mc_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
