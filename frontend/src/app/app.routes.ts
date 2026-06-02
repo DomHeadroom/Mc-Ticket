df68c9937e7970d9d0f4bd85520024b42b238435
DomHeadroom <domefemia@protonmail.com>
2026-06-02 10:35:09 +0200

    [frontend] Aggiunto auth guard
    Signed-off-by: DomHeadroom <domefemia@protonmail.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { authGuard } from './auth/guards/auth-guard';
+import { guestGuard } from './auth/guards/guest-guard';
 
 export const routes: Routes = [
   {
     path: 'login',
+    canMatch: [guestGuard],
     component: Login,
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      { path: 'home', component: Home },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
 ];
