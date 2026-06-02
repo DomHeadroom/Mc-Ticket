c03e75aaca3e74b4b4980a93c5cecb920ae4e284
DomHeadroom <domefemia@protonmail.com>
2026-06-02 15:58:39 +0200

    [backend] aggiunto openapi
    Signed-off-by: DomHeadroom <domefemia@protonmail.com>

--- backend/src/main/java/it/domheadroom/mc_ticket/configuration/SecurityConfig.java
+++ backend/src/main/java/it/domheadroom/mc_ticket/configuration/SecurityConfig.java
@@ -33,7 +33,12 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/auth/**").permitAll()
+                        request.requestMatchers(
+                                        "/auth/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html"
+                                ).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
